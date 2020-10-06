f2301ecabf2c9d0f4e5fd268ca0aecd02b1ebf2f
gbindinazeez <gbindinazeez@gmail.com>
2020-10-06 12:30:38 +0100

    Subcomponent

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,28 @@
+package com.example.android.dagger.di
+
+import android.app.Activity
+import android.content.Context
+import com.example.android.dagger.login.LoginComponent
+import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [StorageModule::class, AppSubComponents::class])
+interface AppComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
+
+    fun inject(activity: MainActivity)
+}
